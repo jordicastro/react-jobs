9d99e0e53e5e95c2b95b98b8e800757436113fb2
Jordi Castro <jordi.castro003@gmail.com>
2024-03-30 17:29:34 -0500

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 const App = () => {
+  const name = 'Jordi';
+  const loggedIn = false;
+  let flowers = ['daisys', 'roses', 'sunflowers'];
+  const styles = {
+    color: 'tomato',
+    fontSize: '24px'
+  }
+
   return (
-    <div>
-      <h1>Hello, world!</h1>
-      <p>Welcome to the React 18 Alpha release.</p>
+    <>
+    <div className='text-5xl'>
+      App
     </div>
+
+    <p style={styles}>hello {name}</p>
+
+    <ul>
+    { flowers.map( (flower, index) => (
+      <li key={index}>i like {flower}</li>
+      ))}
+    </ul>
+
+    {loggedIn ? ( <h1>hello {name}</h1>) : (<h1>hello guest</h1>) }
+
+    </>
   )
-}
+};
 
 export default App
