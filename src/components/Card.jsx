2b6c413a23a0521f03f39f40a4f472acce379ee3
Jordi Castro <jordi.castro003@gmail.com>
2024-03-30 20:12:39 -0500

    raw html => react components finished

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+
+import PropTypes from 'prop-types';
+
+const Card = ({ children, bg='bg-gray-100' }) => {
+    return (
+        <div className={`${bg} bg-gray-100 p-6 rounded-lg shadow-md`}>
+                { children }
+        </div>
+    )
+}
+
+Card.propTypes = {
+    children: PropTypes.node.isRequired,
+    bg: PropTypes.string,
+};
+
+export default Card
