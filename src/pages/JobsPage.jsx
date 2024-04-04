8bb48a387ca6e67c6122cbd8b2f10da33a3e7f5e
Jordi Castro <jordi.castro003@gmail.com>
2024-04-03 20:08:20 -0500

    install json-server => mock backend

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,8 +1,11 @@
+import JobListings from '../components/JobListings';
+
 const JobsPage = () => {
   return (
     <>
-        <h1>Jobs</h1>
-        <p>Here are some jobs</p>
+        <section className='bg-blue-50 px-4 py-6'>
+          <JobListings />
+        </section>
     </>
   );
 }
